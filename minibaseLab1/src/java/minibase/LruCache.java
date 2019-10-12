24858d9748c0d6c665fdae9acb1e5772b88cefd6
Csoyee <ithdli@naver.com>
2019-10-12 21:59:04 +0900

    add skeleton code and testcases for lab4

---
+++ minibaseLab1/src/java/minibase/LruCache.java
@@ -0,0 +1,51 @@
+package minibase;
+
+import java.util.*;
+
+
+/**
+ * @param <K> Type of the Cache Key
+ * @param <V> Type of the Cache Elements
+ */
+public class LruCache <K, V> {
+
+	
+	/**
+	 * Constructor
+	 * @param capacity
+	 */
+	public LruCache (int capacity) {
+	  // TODO: some code goes here 
+	}
+
+	public boolean isCached(K key) {
+	  // TODO: some code goes here 
+	  return false;
+	}
+
+	public V get (K key) {
+	  // TODO: some code goes here 
+	  return null;
+	}
+
+	public void put (K key, V value) {
+	  // TODO: some code goes here 
+	}
+
+	public V evict() {
+	  // TODO: some code goes here 
+	  return null;
+	}
+
+	public int size() {
+	  // TODO: some code goes here 
+	  return -1;
+	}
+	
+	public Iterator<V> iterator() {
+	  // TODO: some code goes here,,, please implement freely! you can remove and make new method 
+	  return null;
+	}
+
+
+}
